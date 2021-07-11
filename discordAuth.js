0b7cc6c33fe12d1f759b5c0d41706f75731b160f
Yakir Ifrah <yakirifrah19@gmail.com>
2021-07-11 19:13:45 +0300

    Create routes for Auth

---
+++ discordAuth.js
@@ -0,0 +1,66 @@
+const router = require('express').Router();
+const jwt = require('jsonwebtoken');
+const btoa = require('btoa');
+const fetch = require('node-fetch');
+
+const redirect = encodeURIComponent('https://login-discord-passport.herokuapp.com/api/auth/discord/redirect');
+router.get('/discord', (req,res)=>{
+	res.redirect(
+		`https://discord.com/api/oauth2/authorize?client_id=${
+			process.env.CLIENT_ID
+		}&scope=${"identify email".replace(
+			/ /g,
+			'%20'
+		)}&response_type=code&redirect_uri=${redirect}`
+	);
+});
+router.get('/callback', async (req,res,next)=>{
+	if (!req.query.code) return next( 'NoCodeProvided');
+	const { code } = req.query;
+	const creds = btoa(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`);
+	const data = {
+		grant_type: 'authorization_code',
+		client_id: 827119255111467008,
+		client_secret: '9WGLqqJ4tQta9wEDhpf5c0qDN5FzDCnO',
+		code,
+		redirect_uri:'https://login-discord-passport.herokuapp.com/api/auth/discord/redirect',
+		scope: "identify email",
+	};
+	const response = await fetch('https://discord.com/api/oauth2/token', {
+		method: 'POST',
+		headers: {
+			Authorization: `Basic ${creds}`,
+			'Content-type': 'application/x-www-form-urlencoded',
+		},
+		body: new URLSearchParams(data),
+	});
+	// eslint-disable-next-line camelcase
+	const { access_token, refresh_token, expires_in } = await response.json();
+	const token = jwt.sign({ id: access_token }, process.env.JWT_SECRET, {
+		expiresIn: expires_in,
+	});
+	const cookieOptions = {
+		// eslint-disable-next-line camelcase
+		expires: new Date(Date.now() + expires_in * 24 * 60 * 60 * 1000),
+	};
+	if (process.env.ENVIRONMENT === 'production') cookieOptions.secure = true;
+
+	res.cookie('jwt', token, cookieOptions);
+	res.redirect(`/success/?token=${access_token}`);
+});
+
+router.get('/me',async(req,res,next)=>{
+	const fetchDiscordUserInfo = await fetch(
+		'https://discord.com/api/users/@me',
+		{
+			headers: {
+				Authorization: `Bearer ${req.query.token}`,
+			},
+		}
+	);
+	const userInfo = await fetchDiscordUserInfo.json();
+	res.status(200).send({data:userInfo});
+})
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,46 +1,22 @@
-require('./discordstrategy');
 
 const express = require('express');
 const http = require('http');
 const passport = require('passport');
-const passportAuth = require('./passportAuth');
+const discordAuth = require('./discordAuth');
 const cookieParser = require('cookie-parser');
 const Store = require('connect-mongo');
-const connectDB = require('./connection');
-const session = require('express-session');
 
 const app = express();
 
 if (process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'));
 }
-app.use(
-	session({
-		secret: 'secret',
-		cookie: {
-			maxAge: 60000 * 60 * 24,
-		},
-		resave: false,
-		saveUninitialized: false,
-		store: new Store({
-			mongoUrl: 'mongodb+srv://dbUser:dbUser@cluster0.7e0w6.mongodb.net/makesApp?retryWrites=true&w=majority',
-		}),
-	}),
-);
-app.use(express.json());
-app.use(cookieParser());
-app.use(passport.initialize());
-app.use(passport.session());
 
-app.use('/api/auth', passportAuth);
-app.get('/', (req, res) => {
-	if (!req?.user) {
-		res.send("Dude you're not authired");
-	}
-	res.send(req.user);
-});
+app.use(express.json());
+
+app.use('/api/auth', discordAuth);
+
 const httpServer = http.createServer(app);
-connectDB().then(() => console.log('DB connected!'));
 httpServer.listen(process.env.PORT || 5000, () => {
 	console.log('welcome to my server');
 });
